623ee160a8b81185818f848a91243d6f479b19df
Arpita <arpitasaraf2@gmail.com>
2025-05-02 15:02:39 +0530

    update

--- Project - Blog/blogger/basic/views.py
+++ Project - Blog/blogger/basic/views.py
@@ -8,9 +8,9 @@ def home_view(request):
     data = Blog.objects.all()
     # print(data.values())
     dict = {
-        "blogs" : data
+        "blogs": data
     }
-    return render(request,'basic/blogCard.html',context=dict)
+    return render(request, 'basic/blogCard.html', context=dict)
 
 
 def create_blog_view(request):
@@ -19,48 +19,70 @@ def create_blog_view(request):
         print(request.POST)
         fm = BlogForm(request.POST)
         # print(fm)
-        if fm.is_valid(): # validate form at server side
-             print("create somse data")
-            #  print(fm.cleaned_data)
-             title = fm.cleaned_data.get('title')
-             content = fm.cleaned_data.get('content')
-             is_published = fm.cleaned_data.get('is_published')
-             create_data = Blog.objects.create(
-                 title = title,
-                 content = content,
-                 is_published = is_published
-             )
-             print(create_data.__dict__)
-             return HttpResponseRedirect(f'/blog-content/{create_data.id}')
+        if fm.is_valid():  # validate form at server side
+            print("create somse data")
+           #  print(fm.cleaned_data)
+            title = fm.cleaned_data.get('title')
+            content = fm.cleaned_data.get('content')
+            is_published = fm.cleaned_data.get('is_published')
+            create_data = Blog.objects.create(
+                title=title,
+                content=content,
+                is_published=is_published
+            )
+            print(create_data.__dict__)
+            return HttpResponseRedirect(f'/blog-content/{create_data.id}')
     else:
         fm = BlogForm()
 
     context = {
-        "form" : fm
+        "form": fm
     }
-    return render(request,'basic/createblog.html',context=context)
+    return render(request, 'basic/createblog.html', context=context)
 
 
-def blog_content_view(request,blog_id,confirm):
-    print("confirm",confirm)
+def blog_content_view(request, blog_id, confirm):
+    print("confirm", confirm)
     print(blog_id)
     data = Blog.objects.get(id=blog_id)
     # print(data.__dict__)
     context = {
-        "blog" : data,
-        "confirm" : confirm
+        "blog": data,
+        "confirm": confirm
     }
-    return render(request,'basic/blogContent.html',context=context)
+    return render(request, 'basic/blogContent.html', context=context)
 
-def delete_blog_view(request,blog_id):
+
+def delete_blog_view(request, blog_id):
     print(request.method)
-    print("eufasdfjklasdjl;",blog_id)
+    print("eufasdfjklasdjl;", blog_id)
     if request.method == "POST":
-        data = Blog.objects.get(id=blog_id) 
+        data = Blog.objects.get(id=blog_id)
         data.delete()
         return HttpResponseRedirect('/')
     else:
-        return render(request,'basic/errorpage.html')
-    
-    
+        return render(request, 'basic/errorpage.html')
 
+
+def update_blog_view(request, blog_id):
+    print("blog : ", blog_id)
+    if request.method == "POST":
+        print(request.POST)
+        data = Blog.objects.get(id=blog_id)
+        fm = BlogForm(request.POST)
+        if fm.is_valid():
+            title = fm.cleaned_data.get("title")
+            content = fm.cleaned_data.get("content")
+            is_published = fm.cleaned_data.get("is_published")
+            print(title, content, is_published)
+            data.title = title
+            data.content = content
+            data.is_published = is_published
+            data.save()
+        #    print(data.__dict__)
+        return HttpResponseRedirect(f'/blog-content/{blog_id}/no')
+    else:
+        fm = BlogForm()
+        data = Blog.objects.get(id=blog_id)
+        context = {"form": fm, "blog": data}
+        return render(request, 'basic/updateblog.html', context=context)
--- Project - Blog/blogger/blogger/urls.py
+++ Project - Blog/blogger/blogger/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from basic.views import home_view,create_blog_view,blog_content_view,delete_blog_view
+from basic.views import home_view, create_blog_view, blog_content_view, delete_blog_view, update_blog_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # basic
-    path('',home_view,name="home_view"),
-    path('create-blog/',create_blog_view,name="create_blog_view"),
-    path('blog-content/<int:blog_id>/<str:confirm>',blog_content_view,name="blog_content_view"),
-    path('delete-blog/<int:blog_id>/',delete_blog_view,name="delete_blog_view")
+    path('', home_view, name="home_view"),
+    path('create-blog/', create_blog_view, name="create_blog_view"),
+    path('blog-content/<int:blog_id>/<str:confirm>',
+         blog_content_view, name="blog_content_view"),
+    path('delete-blog/<int:blog_id>/', delete_blog_view, name="delete_blog_view"),
+    path('update-blog/<int:blog_id>/', update_blog_view, name="update_blog_view")
 ]
