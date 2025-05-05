d66b8ca9c884fc0962abb0d6917c4fb8d279ee62
Arpita <arpitasaraf2@gmail.com>
2025-05-05 16:12:03 +0530

    login & register

--- Project - Blog/blogger/user/views.py
+++ Project - Blog/blogger/user/views.py
@@ -1,12 +1,42 @@
-from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm 
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 # Create your views here.
-
-
+from django.contrib.auth import login
 
 def registration_view(request):
-    return render(request,'user/signup.html')
+    print(request.method)
+    if request.method == "POST":
+        print(request.POST)
+        fm = UserCreationForm(request.POST)
+        print(fm.is_valid())
+        if fm.is_valid():
+            fm.save()
+            return redirect('/login')
+        else:
+            # print(fm.errors)
+            context = {"form": fm}
+            return render(request, 'user/signup.html', context=context)
+    else:
+        fm = UserCreationForm()
+        context = {"form": fm}
+        return render(request, 'user/signup.html', context=context)
 
 
 def login_view(request):
-    return render(request,'user/login.html')
+    # print(request.method)
+    if request.method == "POST":
+        print(request.POST)
+        fm = AuthenticationForm(data=request.POST)
+        print(fm.is_valid())
+        if fm.is_valid():
+            user = fm.get_user()
+            login(request,user)
+            return redirect('/')
+        else:
+            context = {"form": fm}
+            print(fm.errors)
+            return render(request, 'user/login.html', context=context)
+    else:
+        fm = AuthenticationForm()
+        context = {"form": fm}
+        return render(request, 'user/login.html', context=context)
